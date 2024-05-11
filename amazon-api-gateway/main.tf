39c6db935c98b4829d2b2d56a1bc8fb46cdbec8e
Max <devvelejar@gmail.com>
2024-05-11 01:08:56 -0300

    fix

--- amazon-api-gateway/main.tf
+++ amazon-api-gateway/main.tf
@@ -10,43 +10,64 @@ resource "aws_lambda_function" "oauth-cognito" {
   runtime       = "nodejs20.x"
 }
 
-resource "aws_api_gateway_rest_api" "meu_api_gateway" {
-  name        = "meu_api_gateway"
+resource "aws_api_gateway_rest_api" "burger-box" {
+  name        = "burger-box"
   description = "API Gateway para autenticação via Cognito"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.meu_api_gateway.id
+  rest_api_id = aws_api_gateway_rest_api.burger-box.id
 
   depends_on = [aws_api_gateway_integration.integration]
 }
 
 resource "aws_api_gateway_stage" "stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
-  rest_api_id = aws_api_gateway_rest_api.meu_api_gateway.id
+  rest_api_id = aws_api_gateway_rest_api.burger-box.id
   stage_name = "prod"
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.meu_api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.meu_api_gateway.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.burger-box.id
+  parent_id   = aws_api_gateway_rest_api.burger-box.root_resource_id
   path_part   = "auth"
 }
 
 resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.meu_api_gateway.id
+  rest_api_id   = aws_api_gateway_rest_api.burger-box.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_model" "request_model" {
+  rest_api_id = aws_api_gateway_rest_api.burger-box.id
+  name        = "RequestModel"
+  content_type = "application/json"
+
+  schema = jsonencode({
+    "type" = "object",
+    "properties" = {
+      "username" = { "type" = "string" },
+      "password" = { "type" = "string" }
+    },
+    "required" = ["username", "password"]
+  })
+}
+
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.meu_api_gateway.id
+  rest_api_id             = aws_api_gateway_rest_api.burger-box.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.oauth-cognito.invoke_arn
+  request_templates = {
+    "application/json" = jsonencode({
+      "username": "$input.path('$.username')",
+      "password": "$input.path('$.password')"
+    })
+  }
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
@@ -54,6 +75,6 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.oauth-cognito.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:us-east-1:467827799632:${aws_api_gateway_rest_api.meu_api_gateway.id}/*/*/*"
+  source_arn    = "arn:aws:execute-api:us-east-1:467827799632:${aws_api_gateway_rest_api.burger-box.id}/*/*/auth"
 }
 
